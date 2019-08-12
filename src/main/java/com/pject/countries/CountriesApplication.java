c2b34a6e7021f65d987d433f3cdde3c6ecf5f55d
efrain <efrainva98@gmail.com>
2019-08-12 18:51:40 -0400

    i still need to add server methods

---
+++ src/main/java/com/pject/countries/CountriesApplication.java
@@ -0,0 +1,16 @@
+package com.pject.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class CountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesApplication.class, args);
+    }
+
+
+}
